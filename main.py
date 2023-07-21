8d9e479f079c7264a9cc034f399266f437b861cf
Pablo Da costa <pablo.costa>
2023-07-21 04:42:07 -0300

    add: 2 crawled versions

---
+++ main.py
@@ -0,0 +1,13 @@
+import polars
+
+
+df = polars.read_parquet("./data/NTLHNew.parquet")
+
+print(df.filter(polars.col("livro") == "1%20João"))
+
+df2 = polars.read_parquet("./data/ARCNew.parquet")
+
+
+print(df2.filter(polars.col("livro") == "1%20João"))
+
+FAZER PARA OS ESTILOS VFL, NVT NVI-PT E OL
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from nltk import tokenize
 
 
-dictOldBooksVFL = {"Gênesis": 50,
+dictOldBooksNTLH = {"Gênesis": 50,
 "Êxodo": 40,
 "Levítico": 27,
 "Números": 36,
@@ -26,7 +26,49 @@ dictOldBooksVFL = {"Gênesis": 50,
 "Salmos": 150,
 "Provérbios": 31,
 "Eclesiastes": 12,
-"Cânticos": 8,
+"Cântico%20dos%20Cânticos": 8,
+"Isaías": 66,
+"Jeremias": 52,
+"Lamentações%20de%20Jeremias": 5,
+"Ezequiel": 48,
+"Daniel": 12,
+"Oseias": 14,
+"Joel": 3,
+"Amós": 9,
+"Obadias": 1,
+"Jonas": 4,
+"Miqueias": 7,
+"Naum": 3,
+"Habacuque": 3,
+"Sofonias": 3,
+"Ageu": 2,
+"Zacarias":14,
+"Malaquias": 4}
+
+
+
+dictOldBooksARC = {"Gênesis": 50,
+"Êxodo": 40,
+"Levítico": 27,
+"Números": 36,
+"Deuteronômio": 34,
+"Josué": 24,
+"Juízes": 21,
+"Rute": 4,
+"1%20Samuel": 31,
+"2%20Samuel": 24,
+"1%20Reis": 22,
+"2%20Reis": 25,
+"1%20Crônicas": 29,
+"2%20Crônicas": 36,
+"Esdras": 10,
+"Neemias": 13,
+"Ester": 10,
+"Jó": 42,
+"Salmos": 150,
+"Provérbios": 31,
+"Eclesiastes": 12,
+"Cantares": 8,
 "Isaías": 66,
 "Jeremias": 52,
 "Lamentações": 5,
@@ -45,8 +87,35 @@ dictOldBooksVFL = {"Gênesis": 50,
 "Zacarias":14,
 "Malaquias": 4}
 
+dicNewBookARC = {"Mateus": 28,
+"Marcos": 16,
+"Lucas": 24,
+"João": 21,
+"Atos": 26,
+"Romanos": 16,
+"1%20Coríntios": 16,
+"2%20Coríntios": 13,
+"Gálatas": 6,
+"Efésios": 6,
+"Filipenses": 4,
+"Colossenses": 4,
+"1%20Tessalonicenses": 5,
+"2%20Tessalonicenses": 3,
+"1%20Timóteo": 6,
+"2%20Timóteo": 4,
+"Tito": 3,
+"Filemom": 1,
+"Hebreus": 13,
+"Tiago": 5,
+"1%20Pedro": 5,
+"2%20Pedro": 3,
+"1%20João": 5,
+"2%20João": 1,
+"3%20João": 1,
+"Judas": 1,
+"Apocalipse": 22}
 
-dicNewBookVFL = {"Mateus": 28,
+dicNewBookNTLH = {"Mateus": 28,
 "Marcos": 16,
 "Lucas": 24,
 "João": 21,
@@ -75,15 +144,18 @@ dicNewBookVFL = {"Mateus": 28,
 "Apocalipse": 22}
 
 
+def toDict(estilo : list, capitulo : list, livro : list, versiculos : list, textos : list) -> dict:
+    return {"estilo": estilo, "livro": livro,  "capitulo": capitulo, "versiculo": versiculos, "texto": textos}
 
-def crawlSite(url: str, chapter: str) -> dict:
+
+def crawlSite(url: str) -> dict:
     versiculos = {}
     response = requests.get(url)
     htmlContent = response.text
     verses = trafilatura.extract(htmlContent, include_images=False, include_formatting=False)
     words         = tokenize.word_tokenize(verses, language='portuguese')
     isFirstCheck  = 1
-    listVersicles = [str(i) for i in range(np.argmax([int(x) for x in words if x.isnumeric()][2:])+1)]
+    listVersicles = [str(i) for i in range(np.max([int(x) for x in words if x.isnumeric()][2:])+1)]
     verse         = []
     
     for word in words:
@@ -106,10 +178,11 @@ def crawlSite(url: str, chapter: str) -> dict:
             verse.append(word)
 
     #case n
-    versiculos[listVersicles[0]] = " ".join(verse)
-    verse = []
-    listVersicles = listVersicles[1:]
-
+    if len(listVersicles) >= 1:
+        versiculos[listVersicles[0]] = " ".join(verse)
+        verse = []
+        listVersicles = listVersicles[1:]
+    print(versiculos)
     return versiculos
 
 def crawlSite_depreciado(url: str, chapter: str):
