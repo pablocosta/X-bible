458e5e551a53be5f53bec423cc09cdfad5bb1122
Pablo costa <pablo.costa>
2023-07-22 04:42:36 -0300

    add: portuguese crawled bibles

---
+++ NVT.py
@@ -0,0 +1,74 @@
+import utils
+import polars
+import tqdm
+import sys
+
+
+baseUrl = "https://www.biblegateway.com/passage/?"
+
+#PRECISAMOS FAZER UM FOR PARA CADA ESTILO OU VERSÃO
+version      = "NVT"
+
+#url = baseUrl + "search=" + "Mateus" + "%20" + "5" + "&version=" + version
+#versicles = utils.crawlSite(url, 1)
+
+print("Velho Testamento: ")
+#velho testamento
+listDf = []
+versiculos = []
+textos     = []
+estilo     = []
+capitulo   = []
+livro      = []
+for k, v in tqdm.tqdm(utils.dictOldBookNVT.items()):
+    
+    for chapter in range(1, v+1):
+        url = baseUrl + "search=" + k + "%20" + str(chapter) + "&version=" + version
+        print(url)
+        dicionarioUnidade = utils.crawlSite(url)
+        versiculos.extend([k for k,_ in dicionarioUnidade.items()])
+        textos.extend([v for _,v in dicionarioUnidade.items()])
+
+        estilo.extend([version] * len(versiculos))
+        capitulo.extend([chapter] * len(versiculos))
+        livro.extend([k] * len(versiculos))
+        
+        listDf.append(polars.from_dict(utils.toDict(estilo=estilo, livro=livro, capitulo=capitulo, versiculos=versiculos, textos=textos)))
+        versiculos = []
+        textos     = []
+        estilo     = []
+        capitulo   = []
+        livro      = []
+    
+
+polars.concat(listDf).write_parquet("./data/NVTOld.parquet")
+
+print("Novo Testamento: ")
+#novo testamento
+listDf = []
+versiculos = []
+textos     = []
+estilo     = []
+capitulo   = []
+livro      = []
+for k, v in tqdm.tqdm(utils.dicNewBookNVT.items()):
+    
+    for chapter in range(1, v+1):
+        url = baseUrl + "search=" + k + "%20" + str(chapter) + "&version=" + version
+        print(url)
+        dicionarioUnidade = utils.crawlSite(url)
+        versiculos.extend([k for k,_ in dicionarioUnidade.items()])
+        textos.extend([v for _,v in dicionarioUnidade.items()])
+
+        estilo.extend([version] * len(versiculos))
+        capitulo.extend([chapter] * len(versiculos))
+        livro.extend([k] * len(versiculos))
+        
+        listDf.append(polars.from_dict(utils.toDict(estilo=estilo, livro=livro, capitulo=capitulo, versiculos=versiculos, textos=textos)))
+        versiculos = []
+        textos     = []
+        estilo     = []
+        capitulo   = []
+        livro      = []
+
+polars.concat(listDf).write_parquet("./data/NVTNew.parquet")
--- main.py
+++ main.py
@@ -9,5 +9,3 @@ df2 = polars.read_parquet("./data/ARCNew.parquet")
 
 
 print(df2.filter(polars.col("livro") == "1%20João"))
-
-FAZER PARA OS ESTILOS VFL, NVT NVI-PT E OL
