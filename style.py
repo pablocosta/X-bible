702a841910f180de1f73834a6fb5f8db432e8642
Pablo Da costa <pablo.botton.costa@gmail.com>
2024-05-18 22:51:58 -0300

    add:final

---
+++ style.py
@@ -0,0 +1,55 @@
+ 
+#Literal Translation
+
+#Dynamic Equivalent
+
+#Free Translation
+
+dict_styles_pt = {
+"livro": ["ARC", "VFL", "NTLH", "NVT", "NVI-PT", "OL"],
+"forma_traducao": ["literal", "dinâmica", "dinâmica", "dinâmica", "dinâmica", "literal"],
+"linguagem": ["arcaica", "moderna", "moderna", "moderna", "moderna", "arcaica"],
+"formal/informal": ["formal", "informal", "informal", "formal", "formal", "formal"]
+}
+
+dict_styles_it = {
+"livro": ["BDG", "CEI", "LND", "NR1994", "NR2006"],
+"forma_traducao": ["dinâmica", "literal", "literal", "literal", "literal"],
+"linguagem": ["moderna", "moderna", "arcaica", "moderna", "moderna"],
+"formal/informal": ["informal", "formal", "formal", "formal", "formal"]
+}
+
+dict_styles_fr = {
+"livro": ["BDS", "LSG", "NEG1979", "SG21"],
+"forma_traducao": ["dinâmica", "literal", "dinâmica", "dinâmica"],
+"linguagem": ["moderna", "arcaica", "arcaica", "moderna"],
+"formal/informal": ["informal", "formal", "formal", "formal"]
+}
+
+dict_styles_es = {
+"livro": ["LBLA", "JBS", "LBLA", "NBLA", "NBV", "NTV", "CST", "NVI", "PDT", "BLP", "BLPH", "RVA-2015", "RVC", "RVR1960", "RVR1977","RVR1995",
+          "RVA", "SRV-BRG", "TLA"],
+"forma_traducao": ["dinâmica", "literal", "dinâmica", "literal", "livre", "livre", "dinâmica", "dinâmica", "livre", "livre", "livre", "literal", "literal", "literal","literal","literal","literal","literal",
+                   "livre"],
+"linguagem": ["moderna", "arcaica", "moderna", "arcaica", "moderna", "moderna", "moderna","moderna","moderna","moderna","moderna","moderna","moderna", "arcaica", "arcaica", "moderna", "arcaica", "moderna", "moderna"],
+"formal/informal": ["formal", "formal", "formal", "formal","informal", "informal", "formal", "formal", "informal", "informal", "informal", "formal", "formal", "formal", "formal", "formal", "formal", "formal", "informal"]
+}
+
+dict_styles_en ={
+"livro": ["AKJV", "AMPC", "AMP", "ASV", "KJ21", "BRG", "CEB", "CJB", "CEV", "DARBY", "DLNT", "DRA", "ERV", "ESV", "ESVUK",
+          "EXB",  "GNV", "KJV", "LEB", "LSB", "MOUNCE", "MSG", "TLB", "NABRE", "NASB", "NASB1995", "NCB", "NCV", "NIRV",
+          "NIVUK", "NKJV", "YLT"], 
+"forma_traducao": ["literal", "livre", "livre", "literal", "literal", "literal", "dinâmica", "dinâmica", "dinâmica", "literal", 
+                   "literal", "literal", "dinâmica", "literal", "literal", "livre", "literal", "literal", "dinâmica", "literal", 
+                   "dinâmica", "livre", "livre", "dinâmica", "literal", "literal", "dinâmica", "dinâmica", "dinâmica", "dinâmica",  
+                   "literal", "literal"],
+                   
+"linguagem": ["arcaica", "moderna", "moderna", "arcaica", "moderna", "moderna", "moderna",  "moderna", "moderna", "arcaica", "moderna", "arcaica",
+              "moderna", "moderna", "moderna", "moderna", "arcaica", "arcaica", "moderna", "moderna", "moderna", "moderna", "moderna", "moderna"
+               , "moderna", "moderna", "moderna", "moderna", "moderna", "moderna", "moderna", "arcaica"],
+"formal/informal":["formal", "formal", "formal", "formal", "formal", "formal", "formal", "informal", "informal", "formal", "formal", "formal",
+                   "informal", "formal", "formal", "formal", "formal", "formal", "formal", "formal", "formal",  "informal", "informal", "formal",
+                   "formal", "formal", "formal", "formal", "informal", "formal", "formal", "formal"
+                   ]
+                
+}
